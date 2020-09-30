ceb6ab2569b1b024ff8b0928d6b331ab8038c53b
Rayan Castro <castrorry@iMac-de-Rayan.local>
2020-09-30 12:59:01 -0300

    Updates

---
+++ precache-manifest.9b0fd972347d56155b2e7075cfcbcab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c982d8317202a20db7d535845d26b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09a012b8e971dd50738",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e3b4b196271a354d1e5",
+    "url": "/static/js/2.d1042048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1042048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09a012b8e971dd50738",
+    "url": "/static/js/main.f9cf6ca9.chunk.js"
+  },
+  {
+    "revision": "fb32c13a6a2bfd6bbb12",
+    "url": "/static/js/runtime-main.b6d861a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
